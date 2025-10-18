f76fe06e2bd788d38898672503d643ee9ea336e3
Reorso <reorso@hotmail.it>
2025-10-18 13:08:48 +0100

    created a mesh generator with a simple sphere which can be saved

--- LsystemGenerator.gd
+++ LsystemGenerator.gd
@@ -1,84 +1,4 @@
-#extends Node
-#
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-#
-#class LSystem:
-	#var rules
-	#var currentState = ""
-	#
-	#func _getNewGeneration():
-		##this will go through each state and try to apply the rules
-		#while currentState.count() > 0:
-			#foreach rule in rules:
-				#if rule.count() < currentState.count():
-					#currentState
-		#pass
-		
-		
-#extends Node2D
-#
-#class_name Plant
-#
-#var axiom: String = "F"
-#var rules: Dictionary = {
-	#"F": "F[-F]F[+F]F"  # Example rule for a fern-like plant
-#}
-#var angle: float = 25.7
-#var iterations: int = 4
-#var length: float = 100
-#
-#var current_string: String
-#var turtle_stack: Array = []
-#
-#func _ready():
-	#current_string = axiom
-	#for i in range(iterations):
-		#current_string = apply_rules(current_string)
-	##draw_plant(current_string)
-#
-#func apply_rules(string: String) -> String:
-	#var new_string: String = ""
-	#for letter in string:
-		#if letter in rules:
-			#new_string += rules[letter]
-		#else:
-			#new_string += letter
-	#return new_string
-	#
-#func _draw() -> void:
-	#draw_plant(current_string)
-#
-#func draw_plant(string: String):
-	#var turtle_pos: Vector2 = Vector2(0, 0)
-	#var turtle_angle: float = -90  # Pointing upwards initially
-	#turtle_stack.append([turtle_pos, turtle_angle])
-	#
-	#for letter in string:
-		#match letter:
-			#"F":
-				#var new_pos: Vector2 = turtle_pos + Vector2(cos(deg_to_rad(turtle_angle)), sin(deg_to_rad(turtle_angle))) * length
-				#draw_line(turtle_pos, new_pos, Color(0, 1, 0), 2)
-				#turtle_pos = new_pos
-			#"+":
-				#turtle_angle += angle
-			#"-":
-				#turtle_angle -= angle
-			#"[":
-				#turtle_stack.append([turtle_pos, turtle_angle])
-			#"]":
-				#var state = turtle_stack.pop_back()
-				#turtle_pos = state[0]
-				#turtle_angle = state[1]
-				
-extends Node2D
+extends Node3D
 
 class_name Plant
 
@@ -92,13 +12,14 @@ class_name Plant
 var current_string: String
 var turtle_stack: Array = []
 var drawing_commands: Array = []
+var meshes: Array = []
 
 func _ready():
 	current_string = axiom
 	for i in range(iterations):
 		current_string = apply_rules(current_string)
 		print_debug("current iteration, content: ", i, current_string)
-	prepare_drawing_commands(current_string)
+	generate_meshes(current_string)
 	emit_signal("draw")  # Request a redraw
 
 func apply_rules(string: String) -> String:
@@ -122,6 +43,10 @@ func prepare_drawing_commands(string: String):
 				var new_pos: Vector2 = turtle_pos + Vector2(cos(deg_to_rad(turtle_angle)), sin(deg_to_rad(turtle_angle))) * length
 				drawing_commands.append({ "start": turtle_pos, "end": new_pos })
 				turtle_pos = new_pos
+			"X":
+				var new_pos: Vector2 = turtle_pos + Vector2(cos(deg_to_rad(turtle_angle)), sin(deg_to_rad(turtle_angle))) * length
+				#drawing_commands.append({ "start": turtle_pos, "end": new_pos })
+				#turtle_pos = new_pos
 			"+":
 				turtle_angle += angle
 			"-":
@@ -134,7 +59,44 @@ func prepare_drawing_commands(string: String):
 				turtle_angle = state[1]
 
 func _draw():
-	#print_debug("drawing ", drawing_commands)
+	print_debug("drawing ", drawing_commands)
 	for command in drawing_commands:
-		draw_line(command["start"], command["end"], Color(1, 1, 1), 2)
-		draw_circle(command["start"], 3, Color(1, 0, 0))  # Debug circle to mark start points
+		draw_line(command["start"], command["end"])
+
+func draw_line(start: Vector3, end: Vector3):
+	var mesh_instance = MeshInstance3D.new()
+	var mesh = create_cylinder_mesh(length * 0.1, start.distance_to(end))
+	mesh_instance.mesh = mesh
+	mesh_instance.transform.origin = (start + end) / 2
+	add_child(mesh_instance)
+	mesh_instance.look_at(end, Vector3(0, 1, 0))
+	meshes.append(mesh_instance)
+
+func create_cylinder_mesh(radius: float, height: float) -> Mesh:
+	var mesh = CylinderMesh.new()
+	mesh.bottom_radius = radius
+	mesh.top_radius = radius
+	mesh.height = height
+	return mesh
+
+
+func generate_meshes(string: String):
+	var turtle_pos: Vector3 = Vector3(0, 0, 0)
+	var turtle_rot: Basis = Basis()
+
+	for char in string:
+		match char:
+			"F":
+				var new_pos: Vector3 = turtle_pos + turtle_rot * Vector3(0, 0, length)
+				draw_line(turtle_pos, new_pos)
+				turtle_pos = new_pos
+			"+":
+				turtle_rot = turtle_rot.rotated(Vector3(0, 1, 0), deg_to_rad(angle))
+			"-":
+				turtle_rot = turtle_rot.rotated(Vector3(0, 1, 0), deg_to_rad(-angle))
+			"[":
+				turtle_stack.append([turtle_pos, turtle_rot])
+			"]":
+				var state = turtle_stack.pop_back()
+				turtle_pos = state[0]
+				turtle_rot = state[1]
